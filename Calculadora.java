e513c9a90afcefdfd6e4773835692cafded087a0
casoblus <casobrinolusquinos@gmail.com>
2015-12-02 22:18:54 +0100

    Version0

---
+++ Calculadora.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Calculadora
+{
+	private static void SUMA() 
+	{}
+	private static int RESTA() 
+	{}
+	private static int MULTI() 
+	{}
+	private static void DIVI() 
+	{}
+	private static int CAMBIO_S() 
+	{}
+	public static void main( String[] args )
+	{}
+}
