f8e57dd78e033b246a83e24a8a1da441157c36e9
casoblus <casobrinolusquinos@gmail.com>
2015-12-03 01:21:01 +0100

    Instrucciones comentadas

--- Calculadora.java
+++ Calculadora.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
 class Calculadora
 {
+	// SUMA: Le pasamos los dos operandos y visualiza el resultado
 	private static void SUMA() 
 	{}
+	// Le pasamos dos parametros y devuelve el resultado
 	private static int RESTA() 
 	{}
+	// Pedimos los datos en la función y retorna el resultado
 	private static int MULTI() 
 	{}
+	// Pedimos los datos en la funcion y visualiza el resultado
 	private static void DIVI() 
 	{}
+	// Pedimos los datos en la funcion y retorna el resultado
 	private static int CAMBIO_S() 
 	{}
+	// Creamos un menu con las distintas opciones mas una para salir
+	// Elegir una opcion distinta genera ERROR.
 	public static void main( String[] args )
 	{}
 }
